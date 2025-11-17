9f69286d4f234046c9f164c21fb4411fabc99100
Jōji <gpgerov+github@gmail.com>
2025-11-18 00:40:20 +0100

    #28: minor fixes and improvements

--- Components/AttractorComponent/attractor_component.gd
+++ Components/AttractorComponent/attractor_component.gd
@@ -1,24 +1,38 @@
 class_name AttractorComponent
 extends Area2D
 
+## Enables attracting objects from the attractable group
+##
+## For proper use, add a collision shape as a child node to the collectable component. 
+## Objects that use this component can attract other objects that fit the collision layer
+## mask AND are in the attractable group.
+## Adding further collision masks extends who the object can potentially attract.
+
 @export var thing : Node2D
 @export var attraction : float = 150.0
 
 var bodies : Array[Node2D] = []
 
+# adds a body to the bodies array after it enters the attraction area,
+# so it can be moved towards the node passed in "thing"
 func _on_body_entered(body: Node2D) -> void:
-	print("entered attractor component")
 	if body.is_in_group("attractable"):
 		print("found attractable")
 		bodies.append(body)
 
+# removes a body from the bodies array after it exits the attraction area
 func _on_body_exited(body: Node2D) -> void:
-	print("exited attractor component")
 	if body.is_in_group("attractable") and bodies.has(body):
 		bodies.erase(body)
 
+# moves the items that are in the attraction area towards the node passed in "thing"
 func _physics_process(_delta: float) -> void:
-	for body in bodies:
+	for body in bodies.duplicate():
+		if not is_instance_valid(body):
+			bodies.erase(body)
+			continue
+		
 		var direction = body.global_position.direction_to(thing.global_position)
-		body.velocity = direction * attraction
-		body.move_and_slide()
+		
+		if body.has_method("apply_attraction_force"):
+			body.apply_attraction_force(direction * attraction)
--- Components/CollectableComponent/collectable_component.gd
+++ Components/CollectableComponent/collectable_component.gd
@@ -1,9 +1,17 @@
 class_name CollectableComponent
 extends Area2D
 
+## Enables an object to be collected by a collector
+##
+## Objects that use this component can be collected by other objects that fit the collision layer
+## mask AND are in the collector group.
+## Adding further collision masks extends by whom the object can be collected.
+## 
+## For proper use, add a collision shape as a child node to the collectable component. 
+
 @export var thing : Node2D
 
+# currently just deletes the object
 func _on_body_entered(body: Node2D) -> void:
-	print("ENTERED")
 	if body.is_in_group("collector"):
 		thing.queue_free()
