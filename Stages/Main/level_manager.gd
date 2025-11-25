00442f6bf669a5202929340abc81a09cc5a0d816
Jōji <gpgerov+github@gmail.com>
2025-11-25 12:39:08 +0100

    #63: fixed potential bug pits
    - added wrapper for EventBus.play_transition signal that automatically sets a timer thats as long as the fade out time set in the transition_screen -> this ensures that the next map is not loaded in before the transition ends -> no flickering

--- Stages/Main/level_manager.gd
+++ Stages/Main/level_manager.gd
@@ -88,8 +88,7 @@ func _next() -> void:
 		if SaveGameManager.global_data.current_dungeon_level < max_dungeon_level:
 			_go_to_stairs()
 		else:
-			EventBus.play_transition.emit("You defeated", "Gor'Khaz the Crown of Endless Hunger", "After defeating the boss you decided to return to the Village.")
-			await get_tree().create_timer(0.5).timeout  # half-second delay
+			await EventBus.play_transition_event("You defeated", "Gor'Khaz the Crown of Endless Hunger", "After defeating the boss you decided to return to the Village.")
 			SaveGameManager.global_data.current_dungeon_level = 0
 			_go_to_village_entrance()
 	elif SaveGameManager.global_data.current_location == "Stairs":
--- Utilities/event_bus.gd
+++ Utilities/event_bus.gd
@@ -3,3 +3,14 @@ extends Node
 signal shop_open_requested
 signal player_message(message : String)
 signal play_transition(pre_title : String, title : String, message : String)
+
+var transition_screen_path : String = "/root/Main/TransitionScreen"
+
+func play_transition_event(pre_title : String, title : String, message : String) -> void:
+	EventBus.play_transition.emit(pre_title, title, message)
+	
+	if get_tree().root.has_node(transition_screen_path):
+		var transition_screen = get_tree().root.get_node(transition_screen_path)
+		await get_tree().create_timer(transition_screen.background_fade_transition_time).timeout
+	else:
+		await get_tree().create_timer(0.5).timeout
