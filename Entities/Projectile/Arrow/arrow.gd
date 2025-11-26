beeb0a811178ede3fccecf63a43bd59820193da0
Jōji <gpgerov+github@gmail.com>
2025-11-27 00:56:11 +0100

    #90: fixed arrows
    arrows get dynamic collision mask depending on who shoot them

--- Entities/Projectile/Arrow/arrow.gd
+++ Entities/Projectile/Arrow/arrow.gd
@@ -4,21 +4,16 @@ class_name Arrow
 @export var speed := 200.0
 var direction := Vector2.ZERO
 @export var damage : int = 3
-@export var invincible_time := 0.05  
 
 # Nodes
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
-
-var can_damage := false
 
 func _ready():
 	# Ensure the collision shape  is active
 	collision.set_deferred("disabled", false)
 	# connect signal
 	connect("body_entered", Callable(self, "_on_body_entered"))
-	# start timer, after which the arrow can actually make damage
-	_start_invincibility_timer()
 
 func _physics_process(delta):
 	if direction == Vector2.ZERO:
@@ -26,26 +21,21 @@ func _physics_process(delta):
 	global_position += direction * speed * delta
 	rotation = direction.angle()
 
-func _start_invincibility_timer():
-	can_damage = false
-	await get_tree().create_timer(invincible_time).timeout
-	can_damage = true
-
 func _on_body_entered(body: Node) -> void:
-	if not can_damage:
-		return  # still invincible
-
-	if body.is_in_group("enemies"):
+	if body.is_in_group("enemies") or body.is_in_group("player"):
 		if body.has_node("Health"):
 			body.get_node("Health").take_damage(damage)
 		queue_free()  # arrow removed after hit
-
-	if body.is_in_group("player"):
-		if body.has_node("Health"):
-			body.get_node("Health").take_damage(damage)
-		queue_free()
 		return
-	elif not body.is_in_group("player"):
+	else:
 		# if the arrow collides with a wall or non enemy object
 		queue_free()
+		return
+
+func set_collision_layer_and_mask(entity : CharacterBody2D) -> void:
+	if entity.is_in_group("enemies"):
+		set_collision_mask_value(2, true)
+	elif entity.is_in_group("player"):
+		set_collision_mask_value(4, true)
+	else:
 		return
