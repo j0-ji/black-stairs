4496efce1a9c0a21e5807e58eca5d6a9591fcbe6
Jōji <gpgerov+github@gmail.com>
2025-11-27 09:45:39 +0100

    #84: created nice tilesets

--- Entities/Items/Coin/item_coin.gd
+++ Entities/Items/Coin/item_coin.gd
@@ -3,7 +3,7 @@ extends Item
 var global_target_position : Vector2
 
 func _ready() -> void:
-	modulate = Color(0, 0, 0, 0)
+	modulate = Color(1, 1, 1, 0)
 	
 	var tween = create_tween()
 	tween.set_parallel(true)
